166805f0141fdf5b5f727d0d55bb175e6e4e53bf
shessafridi <mshesskhan@gmail.com>
2025-10-02 02:38:39 +0500

    changed default parameters

--- src/lib/fft-params.ts
+++ src/lib/fft-params.ts
@@ -4,10 +4,10 @@ export const getDefaultFFTParams = (): FFTDebugParams => {
   return {
     fftSize: 4096,
     gain: 1,
-    decay: 0.8,
-    smoothingTimeConstant: 0.8,
+    decay: 0.99,
+    smoothingTimeConstant: 0.75,
     windowFunction: 'blackman',
-    bassSensitivity: 2.4,
+    bassSensitivity: 2.2,
     bassBoost: 2.0,
     bassThreshold: 0.3,
   };
